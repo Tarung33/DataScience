9d4bce436a2c376e41ec4c0bffcc4f1d39252629
Tarun G <taruntaru380@gmail.com>
2025-12-23 20:10:25 +0530

    Cors updated

--- server.js
+++ server.js
@@ -20,12 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // CORS configuration
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || 'http://localhost:3000',
-        credentials: true,
-    })
-);
+app.use(cors());
+
 
 // Session configuration
 app.use(
