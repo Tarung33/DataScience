4c19b28b0907d2c445aa26329973a1c677185c8e
Tarun G <taruntaru380@gmail.com>
2025-12-26 21:47:43 +0530

    Error resolved

--- server.js
+++ server.js
@@ -21,7 +21,13 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173'],
+    origin: [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:5173',
+        'http://localhost',
+        'capacitor://localhost'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
